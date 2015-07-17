bb4a4a619aa164276625ca32e8d2579c695fb935
ripazhakgggdkp <3671809+Ripazhakgggdkp@users.noreply.github.com>
2015-07-16 20:07:51 -0500

    Try to tidy up

--- ftb2om2/src/main/java/com/ftb2om2/reader/FtbOldReader.java
+++ ftb2om2/src/main/java/com/ftb2om2/reader/FtbOldReader.java
@@ -8,6 +8,7 @@ package com.ftb2om2.reader;
 import com.ftb2om2.model.BPM;
 import com.ftb2om2.model.DefaultFormat;
 import com.ftb2om2.model.Note;
+import com.ftb2om2.util.ApproximateBpm;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -38,65 +39,43 @@ public class FtbOldReader implements Reader {
     private BufferedReader reader;
     private double mainBpm = 120;
 
-    private double approximateBPM(BufferedReader reader) {
-        //TODO: Redo properly, refactor and make it readable
-        
-        //Tries to find the frequency in the difference of every ms in the file
-        List<Double> noteTimes = new ArrayList<>();
-        boolean bpmFound = false;
-        double approximateBPM = 0;
-        LinkedHashMap<Double, Integer> differenceFrequency = new LinkedHashMap<>();
+    private void readNotes(File file) {
         String readLine;
         try {
+            reader = new BufferedReader(new FileReader(file));
             readLine = reader.readLine();
             while (readLine != null) {
                 if (readLine.startsWith("BPM") || readLine.startsWith("###")) {
                 } else {
                     String[] noteData = readLine.split(" ");
                     String[] noteTimeData = noteData[0].split("-");
-                    Long l = Math.round(Double.parseDouble(noteTimeData[0]));
-                    noteTimes.add(l.doubleValue());
-                }
-                if (noteTimes.size() == 2) {
-                    Double difference = noteTimes.get(1) - noteTimes.get(0);
-                    if (differenceFrequency.get(difference) != null && difference != 0) {
-                        differenceFrequency.put(difference, differenceFrequency.get(difference) + 1);
-                    } else {
-                        differenceFrequency.put(difference, 1);
+                    double beatLength = 0;
+                    double noteTimeStart = (double) Math.round(Double.parseDouble(noteTimeData[0]));
+                    if (noteTimeData.length > 1) {
+                        int noteTimeEnd = (int) Math.round(Double.parseDouble(noteTimeData[1]));
+                        beatLength = noteTimeEnd - noteTimeStart;
                     }
-                    noteTimes.clear();
+                    storage.addNote(new Note(
+                            noteTimeStart,
+                            beatLength,
+                            (short) (Short.parseShort(noteData[2]) - 1)));
                 }
                 readLine = reader.readLine();
             }
-            reader.reset();
+            reader.close();
         } catch (Exception ex) {
         }
-        
-        //Sorts Map to get the most frequent entry        
-        Map<Double,Integer> sorted = differenceFrequency.entrySet().stream()
-                                   .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                                   .limit(1)
-                                   .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-        
-        Map.Entry<Double,Integer> entry = sorted.entrySet().iterator().next();    
-        
-        Double mostFrequentDifference = entry.getKey();
-        
-        double snap = 0.125;
-        
-        //Tries to get a possible bpm in between 100 and 200
-        while (approximateBPM < 100 || approximateBPM > 200 && snap != 32) {
-            approximateBPM = 60000 / (mostFrequentDifference * snap);
-            snap = snap * 2;
-        }
-        return approximateBPM;
     }
 
     @Override
     public void read(File file) throws IOException {
+        String readLine;        
+        
+        readNotes(file);        
+        mainBpm = ApproximateBpm.approximate(storage.getNotes());
+        storage.getNotes().clear();
+        
         reader = new BufferedReader(new FileReader(file));
-        mainBpm = approximateBPM(reader);
-        String readLine;
         while ((readLine = reader.readLine()) != null) {
             if (readLine.startsWith("###")) {
             } else if (readLine.startsWith("BPM")) {
@@ -112,19 +91,7 @@ public class FtbOldReader implements Reader {
                 double noteTimeStart = (double) Math.round(Double.parseDouble(noteTimeData[0]));
                 if (noteTimeData.length > 1) {
                     int noteTimeEnd = (int) Math.round(Double.parseDouble(noteTimeData[1]));
-                    boolean bpmFound = false;
-                    ListIterator<BPM> bpmIterator = storage.getBpms().listIterator(storage.getBpms().size());
-                    while (bpmIterator.hasPrevious()) {
-                        BPM bpm = bpmIterator.previous();
-                        if (bpm.getMs() <= noteTimeStart) {
-                            double noteTimeLength = (noteTimeEnd - noteTimeStart);
-                            beatLength = noteTimeLength * bpm.getBpm() / 60000;
-                            bpmFound = true;
-                        }
-                        if (bpmFound) {
-                            break;
-                        }
-                    }
+                    beatLength = noteTimeEnd - noteTimeStart;
                 }
                 storage.addNote(new Note(
                         noteTimeStart,
